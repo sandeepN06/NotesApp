0fa2255d9e98e29c327023ee5c661cb32177d86b
Sandeep N <sandeepbk01@gmail.com>
2021-02-03 19:22:18 +0530

    Done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from website import  create_app
+  
+from website import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,41 @@
-from flask import Flask 
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
-
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'Ssshh This is secret' #Import like for encryption and cookies thing
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
-                              #Importing the BluePrints for regestering them
     from .auth import auth
 
-    app.register_blueprint(views , url_prefix='/')
-    app.register_blueprint(auth , url_prefix='/') #No prefix as now (19.20)
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
 
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
